f2a59a85fd65dc3600e1a742376f60e5848f8e36
mr.wen <604989282@qq.com>
2018-08-05 16:56:19 +0800

    add alipay

---
+++ src/AliPay/Alipay.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jw\Pay\AliPay;
+
+/**
+ * 阿里的支付类
+ * Class Alipay
+ * @package Jw\Pay\AliPay
+ */
+class Alipay
+{
+    public $payType;
+
+    public function __construct()
+    {
+        $this->payType = 'web';
+
+
+    }
+
+    public function pay()
+    {
+
+    }
+}
